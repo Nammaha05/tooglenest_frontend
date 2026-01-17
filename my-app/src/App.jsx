2d41908960ff58f037080a388f99eee7e6561e67
Nammaha05 <namratamahajan05@gmail.com>
2026-01-17 16:22:39 +0530

    frontend

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRoutes from './routes';
+import './tooglenest_frontend/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
